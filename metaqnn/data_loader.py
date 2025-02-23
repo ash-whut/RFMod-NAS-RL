39bc695a04a8db76736b7053f57eca4846afc758
Ashwin Menon <ashw.men.in@gmail.com>
2025-02-22 23:20:31 -0500

    Changing data loader to only work with RadioML data

--- metaqnn/data_loader.py
+++ metaqnn/data_loader.py
@@ -1,62 +1,20 @@
+import os
 import sys
+from sklearn.model_selection import train_test_split
 
-import h5py
+import h5py as h5
 import numpy as np
 
-
-def load_ches_hd5(database_file,
-                  profiling_traces_file, profiling_metadata_file,
-                  attack_traces_file, attack_metadata_file):
+def load_data(path: str):
     try:
-        in_file = h5py.File(database_file, "r")
+        file_handle = h5.File(path,'r+')
     except ValueError:
-        print(f"Error: can't open HDF5 file '{database_file}' for reading (it might be malformed) ...")
+        print(f"Error: can't open HDF5 file '{path}' for reading (it might be malformed) ...")
         sys.exit(-1)
-    # Load profiling traces
-    x_profiling = np.array(in_file[profiling_traces_file], dtype=np.float64)
-    # Load profiling labels
-    metadata_profiling = np.array(in_file[profiling_metadata_file])
-    # Load attacking traces
-    x_attack = np.array(in_file[attack_traces_file], dtype=np.float64)
-    # Load attacking labels
-    metadata_attack = np.array(in_file[attack_metadata_file])
-    return (x_profiling, metadata_profiling), (x_attack, metadata_attack)
+    x = file_handle['X'][:]
+    y = np.argmax(file_handle['Y'][:], axis=1)
+    z = file_handle['Z'][:]
+    return train_test_split(x, y, z, test_size=0.2, random_state=0)
 
-
-def load_hd5(database_file,
-             profiling_traces_file, profiling_labels_file,
-             attack_traces_file, attack_labels_file,
-             attack_metadata_file=None):
-    try:
-        in_file = h5py.File(database_file, "r")
-    except ValueError:
-        print(f"Error: can't open HDF5 file '{database_file}' for reading (it might be malformed) ...")
-        sys.exit(-1)
-    # Load profiling traces
-    x_profiling = np.array(in_file[profiling_traces_file], dtype=np.float64)
-    # Load profiling labels
-    y_profiling = np.array(in_file[profiling_labels_file])
-    # Load attacking traces
-    x_attack = np.array(in_file[attack_traces_file], dtype=np.float64)
-    # Load attacking labels
-    y_attack = np.array(in_file[attack_labels_file])
-    if attack_metadata_file is None:
-        return (x_profiling, y_profiling), (x_attack, y_attack)
-    else:
-        return (x_profiling, y_profiling), (x_attack, y_attack), in_file[attack_metadata_file]['plaintext']
-
-
-def load_hd5_hw_model(database_file,
-                      profiling_traces_file, profiling_labels_file,
-                      attack_traces_file, attack_labels_file,
-                      attack_metadata_file=None):
-    result = load_hd5(
-        database_file, profiling_traces_file, profiling_labels_file,
-        attack_traces_file, attack_labels_file, attack_metadata_file
-    )
-    y_profiling = np.array([bin(x).count("1") for x in result[0][1]])
-    y_attack = np.array([bin(x).count("1") for x in result[1][1]])
-    if attack_metadata_file is None:
-        return (result[0][0], y_profiling), (result[1][0], y_attack)
-    else:
-        return (result[0][0], y_profiling), (result[1][0], y_attack), result[2]
+def validation_data(x_tst: tuple, y_tst: tuple):
+    return train_test_split(x_tst, y_tst, test_size=0.5, random_state=0)
