ca2c70b14c8f11163a43cb78f326add5db8a8e62
Ashwin Menon <ashw.men.in@gmail.com>
2025-02-23 03:17:38 -0500

    Init TF Runner for custom implementation

--- metaqnn/training/tensorflow_runner.py
+++ metaqnn/training/tensorflow_runner.py
@@ -5,9 +5,9 @@ import numpy as np
 from os import path
 from tqdm import tqdm
 
-from metaqnn.grammar.state_enumerator import State
-from metaqnn.attack import utils
-from metaqnn.training.one_cycle_lr import OneCycleLR
+from grammar.state_enumerator import State
+from attack import utils
+from training.one_cycle_lr import OneCycleLR
 
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
@@ -32,6 +32,7 @@ class TensorFlowRunner(object):
         self.features = self.scale_data(self.features, train_data=True)
         self.test_features = self.scale_data(self.test_features, train_data=False)
         self.validation_features = self.scale_data(self.validation_features, train_data=False)
+        print(self.features)
 
     @staticmethod
     def compile_model(state_list: List[State], loss, metric_list):
@@ -58,27 +59,32 @@ class TensorFlowRunner(object):
         return tf.distribute.MirroredStrategy()
 
     def train_and_predict(self, model, parallel_no=1):
+        print("Training started")
+        print("Training labels shape: ", self.labels.shape)
+        print("Validation labels shape", self.validation_labels.shape)
         model.fit(
             x=self.features[:self.hp.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN], 
             y=self.labels[:self.hp.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN], epochs=self.hp.MAX_EPOCHS,
             batch_size=self.hp.TRAIN_BATCH_SIZE * parallel_no,
-            validation_data=(self.validation_features, self.validation_labels), shuffle=True, callbacks=[
+            validation_data=(self.validation_features[:self.hp.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL], 
+                             self.validation_labels[:self.hp.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL]), shuffle=True, callbacks=[
                 OneCycleLR(
                     max_lr=self.hp.MAX_LR * parallel_no, end_percentage=0.2, scale_percentage=0.1,
                     maximum_momentum=None,
                     minimum_momentum=None, verbose=True
                 ),
                 tf.keras.callbacks.EarlyStopping(
-                    monitor = 'val_loss',
+                    monitor = 'loss',
                     patience = 5,
                     mode='auto',
                     verbose = 1)
             ]
         ) 
+        print("Training finished")
 
         return (
-            model.predict(self.attack_features),
-            model.evaluate(x=validation_features, y=validation_labels, batch_size=self.hp.EVAL_BATCH_SIZE)
+            model.predict(self.test_features),
+            model.evaluate(x=self.validation_features, y=self.validation_labels, batch_size=self.hp.EVAL_BATCH_SIZE)
         )
         
     def scale_data(self, data, train_data: bool):
