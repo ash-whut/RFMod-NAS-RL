efdbd98a9adfd424a0e4ac44840a4d4397b9e362
Ashwin Menon <ashw.men.in@gmail.com>
2025-03-02 22:38:50 -0500

    Updated hyper and state space parameters for run on 2018 dataset

--- metaqnn/models/RML_2021/hyper_parameters.py
+++ metaqnn/models/RML_2021/hyper_parameters.py
@@ -2,24 +2,23 @@ from data_loader import *
 import numpy as np
 
 MODEL_NAME = 'RML_2021'
-DATASET_PATH = '/home/ashwin/datasets/RADIOML_2021_07_INT8/RADIOML_2021_07_INT8.hdf5'
+DATASET_PATH = '/home/ashwin/datasets/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5'
 
 # Number of output neurons
-NUM_CLASSES = 27  # Number of output neurons
+NUM_CLASSES = 24  # Number of output neurons
 
 # Input Size
 INPUT_SIZE = 1024
 
 # Batch Queue parameters
-TRAIN_BATCH_SIZE = 128  # Batch size for training (scaled linearly with number of gpus used)
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 5000  # Number of training examples
-VALIDATION_FROM_ATTACK_SET = True
+TRAIN_BATCH_SIZE = 2048  # Batch size for training (scaled linearly with number of gpus used)
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 1500000  # Number of training examples
 EVAL_BATCH_SIZE = TRAIN_BATCH_SIZE  # Batch size for validation
-NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 1000  # Number of validation examples
+# NUM_EXAMPLES_PER_EPOCH_FOR_EVAL =   # Number of validation examples
 
 
 TEST_INTERVAL_EPOCHS = 1  # Num epochs to test on, should really always be 1
-MAX_EPOCHS = 2  # Max number of epochs to train model
+MAX_EPOCHS = 50  # Max number of epochs to train model
 
 # Training Parameters
 OPTIMIZER = 'Adam'  # Optimizer (should be in caffe format string)
@@ -30,6 +29,7 @@ MAX_LR = 5e-3  # The max LR (scaled linearly with number of gpus used)
 MAX_TRAINABLE_PARAMS_FOR_REWARD = 65000
 
 TRAIN_DATA, TEST_DATA, TRAIN_LABELS, TEST_LABELS, TRAIN_SNRS, TEST_SNRS =  load_data(DATASET_PATH)
-VAL_DATA, UNUSED_TEST_DATA, VAL_LABELS, UNUSED_TEST_LABELS = validation_data(TEST_DATA, TEST_LABELS)
+VAL_DATA, TEST_DATA, VAL_LABELS, TEST_LABELS, VAL_SNRS, TEST_SNRS = val_data_split(TEST_DATA, TEST_LABELS, TEST_SNRS)
+BEST_SNR_VAL_DATA, BEST_SNR_VAL_LABELS = best_snr_data(VAL_DATA, VAL_LABELS, VAL_SNRS)
 
-TRAINED_MODEL_DIR = '../trained_models/'
+TRAINED_MODEL_DIR = 'trained_models/'
--- metaqnn/models/RML_2021/state_space_parameters.py
+++ metaqnn/models/RML_2021/state_space_parameters.py
@@ -1,16 +1,16 @@
-output_states = 27  # Number of Classes/Output layer size
+output_states = 24  # Number of Classes/Output layer size
 input_size = 1024  # Amount of measurements before they enter network/input layer size
 
 layer_limit = 14  # Max number of layers
 
 # Transition Options
 possible_conv_depths = [2, 4, 8]  # Choices for number of filters in a convolutional layer
-possible_conv_sizes = [1, 2, 3]  # Choices for kernel size
-possible_pool_sizes = [2, 4, 7]  # Choices for filter_size for an average pooling layer
+possible_conv_sizes = [1, 2]  # Choices for kernel size
+possible_pool_sizes = [2, 4]  # Choices for filter_size for an average pooling layer
 possible_pool_strides = possible_pool_sizes  # Choices for stride for an average pooling layer
 max_fc = 3  # Maximum number of fully connected layers (excluding final FC layer for softmax output)
 # Possible number of neurons in a fully connected layer
-possible_fc_sizes = [2, 4]
+possible_fc_sizes = [2, 4, 8]
 
 allow_initial_pooling = False  # Allow pooling as the first layer
 init_utility = 0.3  # Set this to around the performance of an average model. It is better to undershoot this
@@ -23,20 +23,16 @@ conv_padding = 'SAME'  # set to 'SAME' (recommended) to pad convolutions so inpu
 # Epsilon schedule for q learning agent.
 # Format : [[epsilon, # unique models]]
 # Epsilon = 1.0 corresponds to fully random, 0.0 to fully greedy
-epsilon_schedule = [[1.0, 300],
-                    [0.9, 20],
-                    [0.8, 20],
-                    [0.7, 20],
-                    [0.6, 25],
-                    [0.5, 25],
-                    [0.4, 25],
-                    [0.3, 25],
-                    [0.2, 25],
-                    [0.1, 25]]
+epsilon_schedule = [[1.0, 200],
+                    [0.8, 50],
+                    [0.6, 70],
+                    [0.4, 70],
+                    [0.2, 70],
+                    [0.1, 40]]
 
 # Q-Learning Hyper parameters
 # Q Learning omega polynomial parameter (α = 1 / t^ω) where t is the iteration step and α is the learning rate from Eq 3
-# This learning rate was based on theoretical and experimental results (Even-Dar and Mansour, 2003)
+# This learning rate was based on theoretical and experimental results (Even-Dar and Mansour, 5003)
 learning_rate_omega = 0.85
 discount_factor = 1.0  # Q Learning discount factor (gamma from Equation 3)
 replay_number = 128  # Number trajectories to sample for replay at each iteration
